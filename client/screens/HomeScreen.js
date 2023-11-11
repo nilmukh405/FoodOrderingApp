b75793459c8574049bde331cbb09c1460120a558
Nilanjana Mukherjee <100830690+nilmukh405@users.noreply.github.com>
2023-11-11 12:44:40 +0530

    Update HomeScreen.js

--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -3,7 +3,7 @@ import React, { useLayoutEffect, useState, useEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import Categories from '../components/categories'
 import FeatureRow from '../components/featuredRow'
-import { getFeaturedResturants } from '../api';
+import { getFeaturedRestaurants } from '../api';
 import * as Icon from "react-native-feather";
 import { themeColors } from '../theme'
 
@@ -15,7 +15,7 @@ export default function HomeScreen() {
       navigation.setOptions({headerShown: false})
     }, [])
     useEffect(()=>{
-        getFeaturedResturants().then(data=>{
+        getFeaturedRestaurants().then(data=>{
             setFeaturedCategories(data);
         })
     },[])
@@ -29,7 +29,7 @@ export default function HomeScreen() {
         <View className="flex-row items-center space-x-2 px-4 pb-2 ">
             <View className="flex-row flex-1 items-center p-3 rounded-full border border-gray-300">
                 <Icon.Search height="25" width="25" stroke="gray" />
-                <TextInput placeholder='Resturants' className="ml-2 flex-1" keyboardType='default' />
+                <TextInput placeholder='Restaurants' className="ml-2 flex-1" keyboardType='default' />
                 <View className="flex-row items-center space-x-1 border-0 border-l-2 pl-2 border-l-gray-300">
                     <Icon.MapPin height="20" width="20" stroke="gray" />
                     <Text className="text-gray-600">New York, NYC</Text>
@@ -62,7 +62,7 @@ export default function HomeScreen() {
                             key={category._id}
                             id={category._id}
                             title={category.name}
-                            resturants={category?.resturants}
+                            restaurants={category?.restaurants}
                             description={category.description}
                             featuredCategory={category._type}
                         />
