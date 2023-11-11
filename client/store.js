2fc4daf18401732fcbbdf57e6724f91af249f1f0
Nilanjana Mukherjee <100830690+nilmukh405@users.noreply.github.com>
2023-11-11 12:54:58 +0530

    Update store.js

--- client/store.js
+++ client/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import basketSlice from './slices/basketSlice'
+import restaurantSlice from './slices/restaurantSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    basket: basketSlice,
+    resturant: restaurantSlice
+  },
 })
